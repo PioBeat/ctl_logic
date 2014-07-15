d870e15ef833eaf5e9171e732ad158ea5a711126
Gianluca Grilletti <grilletti.gianluca@gmail.com>
2014-07-15 14:55:23 +0200

    Sistemata l'implementazione di model dopo la modifica della segnatura

--- model.ml
+++ model.ml
@@ -117,34 +117,88 @@ module Model (Space : SPACE) (Time : TIME) : (MODEL with type space = Space.t
     type t = space_point
     let compare = Space.compare
   end
-
   module StMap = Map.Make(SpacePoint)
-  type st_pointset = time_pointset StMap.t
+  
+  type st = time_pointset StMap.t
+  type st_point = space_point * time_point
 
-  let empty_stpt = StMap.empty
+  let st_compare = fun stpt1 stpt2 ->
+    let (spt1,tpt1) = stpt1 in
+    let (spt2,tpt2) = stpt2 in
+    let comp1 = Space.compare spt1 spt2 in
+    if comp1 = 0
+    then Time.compare tpt1 tpt2
+    else comp1
 
-  let add_stpt = fun sp tp stpset ->
-    try
-      let tpset = time_add tp (StMap.find sp stpset) in
-      StMap.add sp tpset stpset
-    with Not_found ->
-      StMap.add sp (time_add tp time_empty) stpset
+  module StPoint = struct
+    type t = st_point
+    let compare = st_compare
+  end
+  module StSet = Set.Make(StPoint)
+
+  type st_pointset = StSet.t
+
+
+
+
+  let string_of_st_point = fun stpt ->
+    let (spt,tpt) = stpt in
+    Printf.sprintf "(%s;%s)" (string_of_space_point spt) (string_of_time_point tpt)
+
+  let string_of_st_pointset_aux = fun stset ->
+    let st_list = StSet.elements stset in
+    String.concat ", " (List.map string_of_st_point st_list)
+  let string_of_st_pointset = fun stset ->
+    Printf.sprintf "[ %s ]" (string_of_st_pointset_aux stset)
+
+
+
+
+  let st_mem = StSet.mem
+  let st_add = fun spt tpt -> StSet.add (spt,tpt)
+  let st_subset = StSet.subset
+  let st_diff = StSet.diff
+  let st_inter = StSet.inter
+  let st_union = StSet.union
+  let st_filter = StSet.filter
+
+
+
+  let st_empty = StSet.empty
+
+  let st_domain = fun st ->
+    let spt_tset_to_stset = fun spt tset -> time_fold (fun tpt set -> st_add spt tpt set) tset st_empty in
+    let set_of_key_val = (fun spt tset set -> st_union (spt_tset_to_stset spt tset) set) in
+    StMap.fold set_of_key_val st st_empty
+
+  let st_complement = fun stset st -> st_diff (st_domain st) stset
+
+  
+
+  (* let add_stpt = fun sp tp stpset -> *)
+  (*   try *)
+  (*     let tpset = time_add tp (StMap.find sp stpset) in *)
+  (*     StMap.add sp tpset stpset *)
+  (*   with Not_found -> *)
+  (*     StMap.add sp (time_add tp time_empty) stpset *)
 
 
 
   (** Proposizioni **)
   type prop = string
+
   let string_of_prop = fun pr -> pr
  
-  type env = prop -> space_point -> time_pointset
 
-  let empty_env = fun pr sp -> failwith (Printf.sprintf "L'ambiente non è definito su questo input: %s" pr)
+  type prop_env = prop -> st_pointset
+
+  let empty_env = fun pr -> failwith (Printf.sprintf "L'ambiente non è definito su questo input: %s" pr)
 
   let bind = fun pr stpset en ->
-    (fun p sp ->
+    (fun p ->
       if ( p = pr )
-      then StMap.find sp stpset
-      else en p sp
+      then stpset
+      else en p
     )
 
 
