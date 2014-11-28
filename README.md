277f24787c0871805036ed38f1469c2987389aa6
Gianluca Grilletti <igrillo00@gmail.com>
2014-11-28 01:25:30 +0100

    Riorganizzata la repository; modificato il readme

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-CTL Model Checker
-=========
+The <name to choose> model checker
+===================================================
 
-Model checker per la logica ctl e per la logica stl.
+This project is a research prototype, implementing a model checker
+that operates on "space" and "time". A model consist of:
+- A topological quasi-discrete space (the "space" part of the model)
+- A Kripke frame (the "time" part of the model)
+- A set of atomic propositions
+
+The core implementation is independent of the chosen model,
+however, currently the logical language is tailored to a specific
+implementation.
+
+At the moment, the source code is divided in two folders. The folder "stl" contains the last version of the source code and some examples.
+In the folder "sandbox" there are some experiments and various experimental versions.
+
+The prerequisites are the ocaml compiler, and the camlimages library.
+
+To compile, type "make stl" in the subdirectory "stl/last_version"; the tool is named "stl" and can be invoked with 3 arguments. Type "make" to launch a test.
+
+The first argument to "stl" is a the prefix of a set of 24-bits bmp image (for example, "maps/map" stands for "maps/map_*.bmp" where * varies in a set of integers). The second argument is the name of a dot file containing the description of a kripke frame (the "time" graph). See
+"stl/last_version" for an example. The third
+(optional) argument is the name of a file to store formulas defined in run-time (the default name is "formula.fr").
+
+This documentation will be updated as soon as a more stable version of
+the tool and the input language of the model checker will be
+available. 
+
+For more information, please write to 
+
+Vincenzo Ciancia
+Gianluca Grilletti
+
+vincenzoml at gmail dot com
+grilletti.gianluca at gmail dot com
+
---
+++ stl/last_version/BusBranch.ml
@@ -0,0 +1,165 @@
+open Images
+open Graphics
+
+
+
+(* nomi dei files, colori dei bus, dati di input *)
+let baseimagename = "data/edinburgh.bmp"
+let basetimetable = "data/bustimetable.txt"
+
+let imagename = "maps/map"
+let dotname = "data/time.dot"
+
+let agumentvalue = 3
+
+let clr = fun i busnumber ->
+  let r = 155 + (100*i / busnumber) in
+ {
+  r = r;
+  g = 0;
+  b = 0;
+}
+
+
+
+
+
+let save_image img filename =
+  Bmp.save filename [] (Rgb24 img)
+
+let load_image filename =
+  match Bmp.load filename [] with
+  | Rgb24 rgbimg -> rgbimg
+  |  _ -> failwith "Only RGB24 bmp images supported at the moment."
+
+let baseimage = load_image baseimagename
+let basex = baseimage.Rgb24.width
+let basey = baseimage.Rgb24.height
+
+let color_pt = fun (x,y) clr img ->
+  let tmp_img = ref img in
+  let _ =
+    for i = x-2 to x+2 do
+      for j = y-2 to y+2 do
+	if i>=0 && i<=basex && j>=0 && j<=basey
+	then Rgb24.set (!tmp_img) i j clr
+      done
+    done in
+  !tmp_img
+
+
+
+let rec pow = fun x y ->
+  if y=0
+  then 1
+  else x*(pow x (y-1))
+
+
+(* la cifra piu significativa a destra *)
+(* la codifica e' la seguente:
+ 1: ha aspettato il bus 1
+ ...
+ 4: nessun autobus ha aspettato
+*)
+let rec base_b = fun n k b ->
+  if k=0
+  then []
+  else (n mod b)::(base_b (n / b) (k-1) b)
+
+(* controlla che ci siano alpiù k stop per ogni bus*)
+(* restituisce 0 se è tutto ok *)
+let rec wait_limit_aux = fun ln k ar ->
+  let lmax = Array.fold_left (fun x y -> max x y) 0 ar in
+  if lmax <= k
+  then match ln with
+       | [] -> 0
+       | 0::sln -> wait_limit_aux sln k ar
+       | n::sln ->
+	  let _ = Array.set ar n (ar.(n) +1) in
+	  wait_limit_aux sln k ar
+  else 1
+  and wait_limit = fun ln k busnumber -> wait_limit_aux ln k (Array.make (busnumber+1) 0)
+
+let count_occurences = fun l x ->
+  let fl = List.filter (fun y -> y=x) l in
+  List.length fl
+
+let write_dot = fun ch str ->
+  output ch str 0 (String.length str)
+
+let sap = fun ls ->
+  List.fold_left (fun s n -> s^"9"^(string_of_int n)) "" (List.sort (-) ls)
+
+
+
+let model_of_timetable = fun mat busnumber ->
+  (* file dot per la parte temporale *)
+  let dot = open_out dotname in
+  let _ = write_dot dot "digraph {\n" in
+  let old_in = ref [] in
+  let l = Array.length mat.(0) in
+  for i = 0 to l-1 do
+    for j = 0 to (pow (busnumber + 1) i)-1 do
+      (* tl è la rappresentazione in lista di uno stato *)
+      let tl = base_b j i (busnumber+1) in
+      if wait_limit tl agumentvalue busnumber = 0
+      then (
+	(* scrittura del file dot - parte temporale *)
+	let _ = match tl with
+	  | [] -> write_dot dot "0;\n"
+	  | [n] -> write_dot dot ("0 -> 9"^(string_of_int n)^";\n")
+	  | hd::stl -> 
+	     if not (List.mem (sap stl,sap tl) (!old_in))
+	     then (
+	       write_dot dot ((sap stl)^" -> "^(sap tl)^";\n");
+	       old_in := (sap stl,sap tl)::(!old_in)
+	     )
+	in
+	(* creazione e salvataggio dell'immagine - parte spaziale *)
+	if tl = []
+	then
+	  let tmpimg = ref(Rgb24.copy baseimage) in
+	  for i = 0 to (Array.length mat) -1 do
+	    tmpimg := color_pt mat.(i).(0) (clr i busnumber) (!tmpimg)
+	  done;
+	  save_image (!tmpimg) (imagename^"_0.bmp")
+	else if tl = List.sort (-) tl
+	then
+	  let lmo k = (List.length tl) - (count_occurences tl (k+1)) in
+	  let tmpstep = Array.init busnumber lmo in
+	  let tmpimg = ref(Rgb24.copy baseimage) in
+	  for i = 0 to busnumber -1 do
+	    tmpimg:= color_pt mat.(i).(tmpstep.(i)) (clr i busnumber) (!tmpimg)
+	  done;
+	  save_image (!tmpimg) (imagename^"_"^(sap tl)^".bmp")
+      )
+    done
+  done;
+  write_dot dot "}"
+	    
+
+
+
+
+let transpose_matrix = fun mat ->
+  let (h,w) = (Array.length mat,Array.length mat.(0)) in
+  let nmat = Array.make_matrix w h (0,0) in
+  let _ =
+    for i = 0 to w-1 do
+      for j = 0 to h-1 do
+	Array.set nmat.(i) j mat.(j).(i)
+      done
+    done in
+  nmat
+
+
+
+let mat =
+  let ch = open_in basetimetable in
+  let buf = Lexing.from_channel ch in
+  let tmat = ParserBus.main LexerBus.token buf in
+  transpose_matrix tmat
+
+let busnumber = Array.length mat
+
+let _ = model_of_timetable mat busnumber
